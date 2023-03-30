00c973af9988cd1ef424037148857109e79d303f
Rafael Dias Menezes <rdmeneze@gmail.com>
2023-03-30 16:13:03 +0200

    [exercism]: resolution of problem 'binary'
    Signed-off-by: Rafael Dias Menezes <rdmeneze@gmail.com>

---
+++ binary/binary.h
@@ -0,0 +1,13 @@
+#if !defined(BINARY_H)
+#define BINARY_H
+
+#include <cstdint>
+#include <string>
+
+namespace binary {
+
+std::uint64_t convert(const std::string &bin_num);
+
+}  // namespace binary
+
+#endif // BINARY_H
