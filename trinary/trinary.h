484c784e3d01623ab0214d3d6d0e36a9b2fa7877
Rafael Dias Menezes <rdmeneze@gmail.com>
2023-03-30 16:59:57 +0200

    [exercism]: solved problem 'trinary'
    Signed-off-by: Rafael Dias Menezes <rdmeneze@gmail.com>

---
+++ trinary/trinary.h
@@ -0,0 +1,12 @@
+#if !defined(TRINARY_H)
+#define TRINARY_H
+
+#include <string>
+
+namespace trinary {
+
+unsigned int to_decimal(std::string number);
+
+}  // namespace trinary
+
+#endif // TRINARY_H
