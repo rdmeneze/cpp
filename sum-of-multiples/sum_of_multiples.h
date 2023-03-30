18689cbd1eab989523ad3b1bab25779c097b8c20
Rafael Dias Menezes <rdmeneze@gmail.com>
2023-03-30 16:57:09 +0200

    [exercism]: solved problem 'sum-of-multiples'
    Signed-off-by: Rafael Dias Menezes <rdmeneze@gmail.com>

---
+++ sum-of-multiples/sum_of_multiples.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <vector>
+#include <cstdint>
+
+namespace sum_of_multiples {
+std::uint64_t to(std::vector<std::uint32_t> base, std::uint64_t limit);
+}  // namespace sum_of_multiples
+
