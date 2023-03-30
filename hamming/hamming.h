3b52870a198db7657886598ba71e8fdb62bb8712
Rafael Dias Menezes <rdmeneze@gmail.com>
2023-03-30 16:39:25 +0200

    [exercism]: solved problem 'hamming'
    Signed-off-by: Rafael Dias Menezes <rdmeneze@gmail.com>

---
+++ hamming/hamming.h
@@ -0,0 +1,17 @@
+#if !defined(HAMMING_H)
+#define HAMMING_H
+
+#include <string>
+#include <stdexcept>
+#include <algorithm>
+
+namespace hamming {
+
+unsigned int compute(std::string seq_a, std::string seq_b);
+
+
+
+
+}  // namespace hamming
+
+#endif // HAMMING_H
