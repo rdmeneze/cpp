6d15a63916287e01a0b7aa78489114aa91c27eae
Rafael Dias Menezes <rdmeneze@gmail.com>
2023-03-30 16:54:06 +0200

    [exercism]: solved problem 'secret-handshake'
    Signed-off-by: Rafael Dias Menezes <rdmeneze@gmail.com>

---
+++ secret-handshake/secret_handshake.h
@@ -0,0 +1,13 @@
+#if !defined(SECRET_HANDSHAKE_H)
+#define SECRET_HANDSHAKE_H
+
+#include <string>
+#include <vector>
+
+namespace secret_handshake {
+
+std::vector<std::string> commands(const int cmd);
+
+}  // namespace secret_handshake
+
+#endif // SECRET_HANDSHAKE_H
